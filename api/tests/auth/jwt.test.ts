c85db42715757ee26c2fd6b80e400a72c3b06d55
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-02-26 13:06:24 +1100

    test: :white_check_mark: add more JWT test cases

--- api/tests/auth/jwt.test.ts
+++ api/tests/auth/jwt.test.ts
@@ -1,6 +1,7 @@
-import { createToken, getUserIdFromToken } from '../../src/auth/jwt';
+import { createToken, getUserIdFromToken } from "../../src/auth/jwt";
+import { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken";
 
-describe("createToken", () => {
+describe("JWT Token Logic", () => {
   it("should return a token", () => {
     const token = createToken({
       id: "1",
@@ -13,16 +14,48 @@ describe("createToken", () => {
   });
 });
 
-describe("getUserIdFromToken", () => {
-  it("should return the user ID from the token", () => {
-    const token = createToken({
-      id: "1",
-      username: "test",
-      createdAt: Date.now(),
-      email: "",
-      updatedAt: Date.now(),
-    });
-    const userId = getUserIdFromToken(token);
-    expect(userId).toBe("1");
+it("should return the user ID from the token", () => {
+  const token = createToken({
+    id: "1",
+    username: "test",
+    createdAt: Date.now(),
+    email: "",
+    updatedAt: Date.now(),
   });
+  const userId = getUserIdFromToken(token);
+  expect(userId).toBe("1");
+});
+
+it("should throw a TokenExpiredError if the JWT is expired", () => {
+  const token = createToken({
+    id: "1",
+    username: "test",
+    createdAt: Date.now(),
+    email: "",
+    updatedAt: Date.now(),
+  });
+  const userId = getUserIdFromToken(token);
+  expect(userId).toBe("1");
+  // Expire the token
+  jest.spyOn(Date, "now").mockReturnValue(100000000000000);
+  expect(() => {
+    getUserIdFromToken(token);
+  }).toThrow(TokenExpiredError);
+});
+
+it("should throw an error if the JWT is tampered with", () => {
+  const token = createToken({
+    id: "1",
+    username: "test",
+    createdAt: Date.now(),
+    email: "",
+    updatedAt: Date.now(),
+  });
+  const userId = getUserIdFromToken(token);
+  expect(userId).toBe("1");
+  // Tamper with the token
+  const tamperedToken = token + "a";
+  expect(() => {
+    getUserIdFromToken(tamperedToken);
+  }).toThrow(JsonWebTokenError);
 });
