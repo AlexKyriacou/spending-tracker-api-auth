87c41080c86055069010faab812c94739402646b
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-02-26 14:47:17 +1100

    chore: remove old jest setup script

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFiles: ["./jest.setup.js"],
   moduleDirectories: ["./api/node_modules", "./node_modules"],
 };
 
--- api/jest.setup.js
+++
@@ -1,1 +0,0 @@
-process.env.JWT_SECRET = 'your_test_secret';
